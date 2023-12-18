34e10bfd9af6d9d1f976c5264132d6ce1052d113
aaronrubinstein <40306940+aaronrubinstein@users.noreply.github.com>
2023-12-18 02:48:08 +0000

    Updates

--- src/App.svelte
+++ src/App.svelte
@@ -2,6 +2,43 @@
   
 </script>
 
+<header>
+    <h1>Simple, traffic-based pricing</h1>
+    <p>Sign-up for our 30-day trial. No credit card required.</p>
+</header>
+
 <main>
 
 </main>
+
+<style>
+    header {
+        margin-top: 104px;
+        margin-bottom: 92px;
+    }
+    
+    h1 {
+        font-size: 28px;
+        font-weight: 800;
+        text-align: center;
+        color: var(--text-and-cta-bg);
+        margin-bottom: 12px;
+    }
+
+    p {
+        font-size: 15px;
+        font-weight: 600;
+        text-align: center;
+        color: var(--text);
+    }    
+    
+    main {
+        width: 540px;
+        height: 397px;
+        border-radius: 8px;
+        background: var(--card-bg);
+        border-radius: 8px;
+        box-shadow: 0px 20px 30px -5px rgba(127, 137, 185, 0.15);
+        padding: 40px 48px 33px 48px;
+    }
+</style>
